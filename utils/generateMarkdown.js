1d40b49bbcada2bfb05f12b1c1267b2eea35f86b
Kristy Thompson <kristy.h.thompson@gmail.com>
2024-10-13 19:32:14 -0600

    Worked on the renderLicenseBadge function. I should add in more badge options in the future

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,7 +15,16 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) return '';
+
+  const links = { 
+    'MIT': 
+
+  }
+return `
+`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
