9604a225fc3f2162ccd61f5d4e03a3950edb36ac
Kristy Thompson <kristy.h.thompson@gmail.com>
2024-10-13 19:03:05 -0600

    Worked on adding liscense badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) return '';
+  
+  const licenses = {
+      'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+      'Apache 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+      'GPL 3.0': 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
+      'ISC': 'https://img.shields.io/badge/License-ISC-blue.svg'
+  };
+
+  return `![License](${licenses[license]})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
